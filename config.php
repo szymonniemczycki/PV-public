5c5f7b02006f1979210410e6e875782374630ce0
szymon.niemczycki <szymon.niemczycki@gmail.com>
2024-03-17 22:42:41 +0100

    fix css - rwd for all

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'user' => 'serwer257855',
+        'password' => 'SzyNie1989!?!'
+        ]
+	];
+
+?>
